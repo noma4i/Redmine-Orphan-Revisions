26182000539aba9808c6657ba7808bbf9139dfc6
noma4i <noma4i@gmail.com>
2013-05-07 15:52:09 +1000

    Adding split tables for Orphan revisions and revisions from issues not included in current version

--- app/controllers/redmine_orphan_revisions_controller.rb
+++ app/controllers/redmine_orphan_revisions_controller.rb
@@ -5,11 +5,14 @@ class RedmineOrphanRevisionsController < ApplicationController
     @project = Project.find(params[:project_id])
     @version = Version.find(params[:version])
 
-    issues_in_release = @version.fixed_issues.map(&:id).join(',')
+    issues_in_release = @version.fixed_issues.map(&:id).join(',') + "0"
+    revisions = Changeset.where("committed_on > ? and committed_on < ?",params[:from],params[:to]).joins("left join changesets_issues ci on ci.changeset_id = changesets.id")
 
     if issues_in_release.size > 0
-      @revisions = Changeset.where("committed_on > ? and committed_on < ?",params[:from],params[:to]).joins("left join changesets_issues ci on ci.changeset_id = changesets.id").where("ci.issue_id is null or ci.issue_id not in (#{issues_in_release})")
+      @revisions_in_wrong_version = revisions.where("ci.issue_id is not null and ci.issue_id not in (#{issues_in_release})")
     end
+
+    @orphan_revisions = revisions.where("ci.issue_id is null")
 
     render 'revisions'
   end
